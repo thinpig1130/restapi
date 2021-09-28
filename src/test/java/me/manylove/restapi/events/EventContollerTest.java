111242a42fbb2bd02aacf9c38604b919c3ca85e2
thinpig1130 <58329774+thinpig1130@users.noreply.github.com>
2021-09-28 16:45:25 +0900

    입력값 제한 Bad_Request로 응답
    약속된 값의 이외의 데이터 Bad_Request로 응답

--- src/test/java/me/manylove/restapi/events/EventContollerTest.java
+++ src/test/java/me/manylove/restapi/events/EventContollerTest.java
@@ -5,12 +5,9 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -18,7 +15,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
-import java.util.Random;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -35,9 +31,77 @@ class EventContollerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+//    @Test
+//    @DisplayName("Event 생성 API 확인 / 받기로 한 값 이외의 무시")
+//    public void createEvent() throws Exception {
+//
+//        Event event = Event.builder()
+//                .id(100)
+//                .name("Spring")
+//                .description("Rest API Development with Spring")
+//                .beginEventDateTime(LocalDateTime.of(2021, 9, 28, 14, 47 ))
+//                .closeEnrollmentDateTime(LocalDateTime.of(2021, 9, 28, 14, 47))
+//                .beginEventDateTime(LocalDateTime.of(2021, 9, 28, 14, 47))
+//                .endEventDateTime(LocalDateTime.of(2021, 9, 28, 14, 47))
+//                .basePrice(100)
+//                .maxPrice(200)
+//                .limitOfEnrollment(100)
+//                .location("강남역 D2 스타텁 팩토리")
+//                .free(true)
+//                .offLine(false)
+//                .build();
+//
+//        // .perform(요청 생성)
+//        // .andExpect(기대하는 결과)
+//        mockMvc.perform(post("/api/events")
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .accept(MediaTypes.HAL_JSON)
+//                    .content(objectMapper.writeValueAsString(event)))
+//                .andDo(print())
+//                .andExpect(status().isCreated())
+//                .andExpect(jsonPath("id").exists())
+//                .andExpect(header().exists(HttpHeaders.LOCATION))
+//                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+//                .andExpect(jsonPath("id").value(Matchers.not(100)))
+//                .andExpect(jsonPath("free").value(Matchers.not(true)))
+//                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+////                .andExpect(jsonPath("offLine").value(Matchers.not(false)));
+//    }
+
     @Test
     @DisplayName("Event 생성 API 확인")
-    public void createEvent() throws Exception {
+    public void createEventDto() throws Exception {
+
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("Rest API Development with Spring")
+                .beginEventDateTime(LocalDateTime.of(2021, 9, 28, 14, 47 ))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021, 9, 28, 14, 47))
+                .beginEventDateTime(LocalDateTime.of(2021, 9, 28, 14, 47))
+                .endEventDateTime(LocalDateTime.of(2021, 9, 28, 14, 47))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    @DisplayName("Event 생성 API 오류 확인")
+    public void createEventError() throws Exception {
 
         Event event = Event.builder()
                 .id(100)
@@ -55,20 +119,11 @@ class EventContollerTest {
                 .offLine(false)
                 .build();
 
-        // .perform(요청 생성)
-        // .andExpect(기대하는 결과)
         mockMvc.perform(post("/api/events")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaTypes.HAL_JSON)
-                    .content(objectMapper.writeValueAsString(event)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
-//                .andExpect(jsonPath("offLine").value(Matchers.not(false)));
+                .andExpect(status().isBadRequest());
     }
 }
